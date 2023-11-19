74eeb3f9890f735d288c857f6e35d325b1706153
Patric Stout <github@truebrain.nl>
2023-11-19 11:15:47 +0100

    feat: add custom attributes for eHP (for ship, hull, armor and shield) (#16)
    eHP is the amount of effective HP a ship has. This is the HP multiplied
    by the resonance, depending on a damage-profile.
    
    Currently the damage-profile is hardcoded to be uniform.

--- src/calculate/pass_4.rs
+++ src/calculate/pass_4.rs
@@ -5,6 +5,7 @@ pub struct PassFour {}
 
 mod align_time;
 mod cpu_power;
+mod ehp;
 mod scan_strength;
 
 #[allow(non_camel_case_types)]
@@ -15,18 +16,45 @@ pub enum AttributeId {
     powerUsed = -4,
     cpuUnused = -5,
     powerUnused = -6,
+    #[allow(dead_code)]
+    velocityBoost = -7, // Is taken care of with effects.
+    shieldEhpMultiplier = -8,
+    armorEhpMultiplier = -9,
+    hullEhpMultiplier = -10,
+    shieldEhp = -11,
+    armorEhp = -12,
+    hullEhp = -13,
+    ehp = -14,
 
     mass = 4,
+    hp = 9,
     powerOutput = 11,
     power = 30,
     cpuOutput = 48,
     cpu = 50,
     agility = 70,
 
+    kineticDamageResonance = 109,
+    thermalDamageResonance = 110,
+    explosiveDamageResonance = 111,
+    emDamageResonance = 113,
+
     scanRadarStrength = 208,
     scanLadarStrength = 209,
     scanMagnetometricStrength = 210,
     scanGravimetricStrength = 211,
+
+    shieldCapacity = 263,
+    armorHp = 265,
+
+    armorEmDamageResonance = 267,
+    armorExplosiveDamageResonance = 268,
+    armorKineticDamageResonance = 269,
+    armorThermalDamageResonance = 270,
+    shieldEmDamageResonance = 271,
+    shieldExplosiveDamageResonance = 272,
+    shieldKineticDamageResonance = 273,
+    shieldThermalDamageResonance = 274,
 }
 
 impl Ship {
@@ -47,5 +75,13 @@ impl Pass for PassFour {
         cpu_power::attribute_power_used(ship);
         cpu_power::attribute_cpu_unused(ship);
         cpu_power::attribute_power_unused(ship);
+
+        ehp::attribute_shield_ehp_multiplier(ship);
+        ehp::attribute_armor_ehp_multiplier(ship);
+        ehp::attribute_hull_ehp_multiplier(ship);
+        ehp::attribute_shield_ehp(ship);
+        ehp::attribute_armor_ehp(ship);
+        ehp::attribute_hull_ehp(ship);
+        ehp::attribute_ehp(ship);
     }
 }
